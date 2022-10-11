0b5b0e27720b3cdfb9537794ec7163985ba383e7
John-Corn <joonatan.kytonen@gmail.com>
2022-10-11 13:47:50 +0300

    more stuff added

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 5,
+        text: "Lorem impsum dolor sit amet consectetur adipsicing elit."
+    },
+    {
+        id: 2,
+        rating: 8,
+        text: "Lorem impsum dolor sit amet consectetur adipsicing elit."
+    },
+    {
+        id: 3,
+        rating: 10,
+        text: "Lorem impsum dolor sit amet consectetur adipsicing elit."
+    }
+];
+
+export default FeedbackData;
